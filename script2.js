bb8a9a5a368f1f2e5569588fd7f5afabe47cd348
github_username <am.company.edu@gmail.com>
2023-07-26 22:43:12 -0500

    cambio de interfas

--- script2.js
+++ script2.js
@@ -29,6 +29,14 @@ function determineWinner(userChoice, computerChoice) {
   }
 }
 
+// Función para manejar la elección del usuario
+function handleChoiceClick(event) {
+  const userChoice = event.target.id;
+  const computerChoice = getComputerChoice();
+  const result = determineWinner(userChoice, computerChoice);
+  showResult(userChoice, computerChoice, result);
+}
+
 // Función para mostrar el resultado y actualizar el puntaje
 function showResult(userChoice, computerChoice, result) {
   resultDiv.innerHTML = `<p>Tu elección: ${userChoice}</p>
@@ -45,24 +53,67 @@ function showResult(userChoice, computerChoice, result) {
   // Mostrar el puntaje actualizado
   playerScoreSpan.textContent = playerScore;
   computerScoreSpan.textContent = computerScore;
-}
 
-// Función para reiniciar el juego
+ /*  // Verificar si el jugador ha ganado 5 veces*/
+  if (playerScore === 5) {
+    resultDiv.innerHTML += "";
+    gameInProgress = false; // Juego terminado
+    showResultMessage("¡Eres ganador!");
+  } else if (computerScore === 5) {
+    resultDiv.innerHTML += "";
+    gameInProgress = false; // Juego terminado
+    showResultMessage("¡Perdiste!");
+  } 
+
+
+  // Función para mostrar el mensaje de resultado centrado en la pantalla
+  function showResultMessage(message) {
+    const resultBox = document.querySelector('.result-box');
+    resultBox.innerHTML = `<p>${message}</p>`;
+    resultBox.style.display = 'block';
+  }
+
+  // Desactivar los botones de elección si el juego ha terminado
+  if (!gameInProgress) {
+    const choiceButtons = document.querySelectorAll('.choice');
+    choiceButtons.forEach(button => {
+      button.disabled = true;
+    });
+  }
+}
 function restartGame() {
   playerScore = 0;
   computerScore = 0;
   playerScoreSpan.textContent = playerScore;
   computerScoreSpan.textContent = computerScore;
   resultDiv.textContent = '';
+  resultDiv.innerHTML = ''; // Limpiar contenido del cuadro del resultado
+
+  // Ocultar el cuadro del resultado
+  const resultBox = document.querySelector('.result-box');
+  resultBox.style.display = 'none';
+
+  // Reactivar los botones de elección
+  const choiceButtons = document.querySelectorAll('.choice');
+  choiceButtons.forEach(button => {
+    button.disabled = false;
+  });
+
+  // Reiniciar el juego en curso
+  gameInProgress = true;
+
+  /* // Reactivar los botones de elección
+  const choiceButtons = document.querySelectorAll('.choice');
+  choiceButtons.forEach(button => {
+    button.disabled = false;
+  });
+
+  // Reiniciar el juego en curso
+  gameInProgress = true; */
 }
 
-// Función para manejar la elección del usuario
-function handleChoiceClick(event) {
-  const userChoice = event.target.id;
-  const computerChoice = getComputerChoice();
-  const result = determineWinner(userChoice, computerChoice);
-  showResult(userChoice, computerChoice, result);
-}
+
+
 
 // Agregar detectores de eventos a todos los botones de elección
 choices.forEach(choice => choice.addEventListener('click', handleChoiceClick));
